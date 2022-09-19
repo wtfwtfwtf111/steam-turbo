9b63e7fbed58263bacb4ee3e4d9172b859de6c2a
Jacob <61052022+Airiuwu@users.noreply.github.com>
2022-09-19 02:58:54 -0400

    fixed

--- terbowe.js
+++ terbowe.js
@@ -16,93 +16,91 @@ var targetID = "vanityURL"; // ID to turbo.
 
 var targetString = "No match";
 var rl = ReadLine.createInterface({
-"input": process.stdin,
-"output": process.stdout
+    "input": process.stdin,
+    "output": process.stdout
 });
 
-doLogin(targetUser, targetPass); 
+doLogin(targetUser, targetPass);
 
 function jetEngine() {
-request("http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=" + apiKey + "&vanityurl=" + targetID, function(error, response, body) {
- if(body.indexOf(targetString) > -1) {
-claim();
-}
-else {
-cnt++;
-if (debug > 0) {
-console.log(cnt);
-}
-}
-});
+    request("http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=" + apiKey + "&vanityurl=" + targetID, function(error, response, body) {
+        if (body.indexOf(targetString) > -1) {
+            claim();
+        } else {
+            cnt++;
+            if (debug > 0) {
+                console.log(cnt);
+            }
+        }
+    });
 }
 
 
 
 function doLogin(accountName, password, authCode, twoFactorCode, captcha) {
-community.login({
-"accountName": accountName,
-"password": password,
-"authCode": authCode,
-"twoFactorCode": twoFactorCode,
-"captcha": captcha
-}, function(err, sessionID, cookies, steamguard) {
-if(err) {
-if(err.message == 'SteamGuard') {
-rl.question("Steam Guard (EMAIL): ", function(code) {
-doLogin(accountName, password, code);
-});
+    community.login({
+        "accountName": accountName,
+        "password": password,
+        "authCode": authCode,
+        "twoFactorCode": twoFactorCode,
+        "captcha": captcha
+    }, function(err, sessionID, cookies, steamguard) {
+        if (err) {
+            if (err.message == 'SteamGuard') {
+                rl.question("Steam Guard (EMAIL): ", function(code) {
+                    doLogin(accountName, password, code);
+                });
 
-return;
-}
-if(err.message == 'SteamGuardMobile') {
+                return;
+            }
+            if (err.message == 'SteamGuardMobile') {
 
-rl.question("Steam Guard (MOBILE): ", function(code) {
-doLogin(accountName, password, null, code);
-});
+                rl.question("Steam Guard (MOBILE): ", function(code) {
+                    doLogin(accountName, password, null, code);
+                });
 
-return;
-}
-if(err.message == 'CAPTCHA') {
-console.log(err.captchaurl);
-rl.question("CAPTCHA: ", function(captchaInput) {
-doLogin(accountName, password, null, captchaInput);
-});
+                return;
+            }
+            if (err.message == 'CAPTCHA') {
+                console.log(err.captchaurl);
+                rl.question("CAPTCHA: ", function(captchaInput) {
+                    doLogin(accountName, password, null, captchaInput);
+                });
 
-return;
-}
+                return;
+            }
 
-console.log(err);
-process.exit();
-return;
-} 
+            console.log(err);
+            process.exit();
+            return;
+        }
 
-console.log("Started turboing " + targetID + " to account " + accountName);
+        console.log("Started turboing " + targetID + " to account " + accountName);
 
 
-setInterval(jetEngine, pDelay);
+        setInterval(jetEngine, pDelay);
 
-});
+    });
 
 }
 
 function setClaim() {
-community.editProfile({
-       "customURL": targetID
-   }, function(err){
-       if (err) {
+    community.editProfile({
+        "customURL": targetID
+    }, function(err) {
+        if (err) {
 
-}
-       else {
-         console.log("Successfully Turboed!");
-       }
-});
+        } else {
+            console.log("Successfully Turboed!");
+        }
+    });
 }
 var claim = (function() {
-   var executed = false;
-   return function() {
-       if (!executed) {
-           executed = true;
-           setClaim();
-}
-   };
+    var executed = false;
+    return function() {
+        if (!executed) {
+            executed = true;
+            setClaim();
+        }
+    };
 })();
